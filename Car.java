997d7ae1632bd873eb35d072afca6b9baab3979a
OGerison <56107729+OGerison@users.noreply.github.com>
2020-11-28 10:20:45 +0300

    Update Car.java

--- Car.java
+++ Car.java
@@ -6,6 +6,9 @@ package Otim;
  public class Car extends Vehicle
     {
         boolean wintertires = false;
+        Car (boolean winter){
+            wintertires = winter;
+        }
         public String toString()
         {
             return "THIS VEHICLE IS "+Color+", HAS WINTER TYRES: "+wintertires;  
